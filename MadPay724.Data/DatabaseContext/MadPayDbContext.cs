9d21a7eeaa915fcf4221e7d33cfc4477787340b6
MohammadRidha mousavi <mosavi.smr@gmail.com>
2020-02-02 20:21:30 +0330

    created Models Of EntityBas&User&Photo&BankCard in Data Layer

---
+++ MadPay724.Data/DatabaseContext/MadPayDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadPay724.Data.DatabaseContext
+{
+    class MadPayDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.; Initial Catalog=MadPay724db; Integrated Security=True; MultipleActiveResultSets=True;");
+        }
+
+    }
+}
---
+++ MadPay724.Data/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadPay724.Data.Models
+{
+    public class User : BaseEntit<string>
+    {
+        public User()
+        {
+            //chon teedade userha mire bala int va long be kare ma nemiad
+            //leza az GUid estefade mikonim
+
+            Id = Guid.NewGuid().ToString();
+            DateModified = DateTime.Now;
+            DateCreated = DateTime.Now;
+        }
+
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        
+
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; }
+        public string City { get; set; }
+
+        public bool IsActive { get; set; }
+        public bool Status { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+        public ICollection<BankCard> BankCards { get; set; }
+
+
+
+
+
+    }
+}
